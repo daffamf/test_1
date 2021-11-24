ccf520d62ded5cba89bc00a2cbeb94f731c822e9
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-11-24 12:09:27 +0700

    algoritma test

--- Algoritma_test/soal.3.js
+++
@@ -1,4 +0,0 @@
-INPUT = ['xc', 'dz', 'bbb', 'dz']  
-QUERY = ['bbb', 'ac', 'dz']  
-
-// OUTPUT = [1, 0, 2] karena kata 'bbb' terdapat 1 pada INPUT, kata 'ac' tidak ada pada INPUT, dan kata 'dz' terdapat 2 pada INPUT
